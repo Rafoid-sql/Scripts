3be73ecce58184b40cf436671a38d082961b1f8f
Rafael Oliveira <rafael.oliveira@sprint.com>
2025-10-02 10:00:37 -0300

    updates

--- PROXY_USER.sql
+++ PROXY_USER.sql
@@ -1,22 +1,13 @@
 qatpaadmx0003:qadmp3:/home/oracle>sqlplus / as sysdba
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 alter user ADM_QLAB01 grant connect through OPS$ORACLE;
 alter user ADM_QLAB02_VIEW grant connect through OPS$ORACLE;
-=======
 alter user ADM_QLAB02_VIEW grant connect through OPS$ORACLE;
 alter user ADM_VIEW grant connect through OPS$ORACLE;
->>>>>>> Stashed changes
 alter user ADM_ZLAB10 grant connect through  
-=======
-=======
->>>>>>> Stashed changes
 alter user ADM_QLAB02_VIEW grant connect through OPS$ORACLE;
 alter user ADM_VIEW grant connect through OPS$ORACLE;
 alter user ADM_ZLAB10 grant connect through OPS$ORACLE;
->>>>>>> Stashed changes
 alter user ADM_QLAB07 grant connect through OPS$ORACLE;
 
 
@@ -39,61 +30,18 @@ SQL> show user
 USER is "ADM_QLAB01"
 SQL>
 
-
-<<<<<<< Updated upstream
-sqlplus [SCMSA_ABC]/@qadp06_aws
+sqlplus [SCH_IOT]/@qadp06_aws
 sqlplus [SCMSA_HIST]/@qids06_aws
 sqlplus [SCMSA_ABC]/@qsaa06_aws
 sqlplus [SCMSA_ABC]/@qsaaup06_aws
-=======
 
-
-
-
-
-
-
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-sqlplus [VSTAPPO]/@qadp06_aws
-sqlplus [SPRINT_COMM]/@qids06_aws
-=======
 sqlplus [SCMSA_LAND]/@qadp06_aws
-sqlplus [DCS_HIST]/@qids06_aws
->>>>>>> Stashed changes
-=======
-sqlplus [SCMSA_LAND]/@qadp06_aws
-sqlplus [DCS_HIST]/@qids06_aws
->>>>>>> Stashed changes
+sqlplus [SCMSA_HIST]/@qids06_aws
 sqlplus [SCMSA_ABC]/@qsaa06_aws
 sqlplus [SCMSA_ABC]/@qsaaup06_aws
 
 
-
-
-
-
 sqlplus [DCS_HIST]/@qids06_aws
-
 sqlplus [DVARI_HIST]/@qids06_aws
-
 sqlplus [SCMSA_HIST]/@qids06_aws
 
-
-SQL> show errors
-Errors for PACKAGE BODY SCMSA_HIST.ECS_SUB_ACTIVITY_PKG:
-
-LINE/COL ERROR
--------- -----------------------------------------------------------------
-2090/3   PL/SQL: SQL Statement ignored
-2090/25  PL/SQL: ORA-00942: table or view does not exist
-2172/4   PL/SQL: SQL Statement ignored
-2179/14  PL/SQL: ORA-00942: table or view does not exist
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
